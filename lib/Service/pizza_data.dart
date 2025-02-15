8a449a7f3f91f7369011f6d135f6cc2e912a4851
Misbah <misbahmuneer2003@gmail.com>
2025-02-15 15:02:38 +0500

    Update

---
+++ lib/Service/pizza_data.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/foundation.dart';
+import 'package:foodgo/Model/pizza_model.dart';
+
+List<PizzaModel> getPizza() {
+  List<PizzaModel> pizza = [];
+
+  PizzaModel pizzaModel = new PizzaModel();
+
+  pizzaModel.name = "Cheese Pizza";
+  pizzaModel.image = "assets/images/pizza1.jpg";
+  pizza.add(pizzaModel);
+  pizzaModel = new PizzaModel();
+}
