717358af75c4ffabe5d99d44d46162046b936f6b
Misbah Haque <misbahmuneer2003@gmail.com>
2025-02-17 12:10:34 +0500

    Update

--- lib/Pages/detail_page.dart
+++ lib/Pages/detail_page.dart
@@ -10,6 +10,31 @@ class DetailPage extends StatefulWidget {
 class _DetailPageState extends State<DetailPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container(child: Column(children: [])));
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.only(top: 40, left: 20),
+        child: Column(
+          children: [
+            GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Container(
+                padding: EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  color: Color(0xffef2b39),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Icon(
+                  Icons.arrow_back_ios_new_rounded,
+                  size: 30,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:foodgo/Pages/detail_page.dart';
 import 'package:foodgo/Pages/home.dart';
 import 'package:foodgo/Pages/onBoarding.dart';
 import 'package:foodgo/bottomnav.dart';
@@ -12,6 +13,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: BottomNav());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: DetailPage());
   }
 }
