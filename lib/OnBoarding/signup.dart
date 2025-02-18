0453cd7a96f8626b572c018f0fd379c56bdc7b30
Misbah Haque <misbahmuneer2003@gmail.com>
2025-02-18 10:10:43 +0500

    Update

--- lib/OnBoarding/signup.dart
+++ lib/OnBoarding/signup.dart
@@ -52,164 +52,167 @@ class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Stack(
-          children: [
-            Container(
-              height: MediaQuery.of(context).size.height / 2.5,
-              padding: EdgeInsets.only(top: 50),
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                color: Color(0xffffefbf),
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(40),
-                  bottomRight: Radius.circular(40),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Container(
+          child: Stack(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height / 2.5,
+                padding: EdgeInsets.only(top: 50),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: Color(0xffffefbf),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(40),
+                    bottomRight: Radius.circular(40),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Image.asset(
+                      "assets/images/pan.png",
+                      height: 180,
+                      fit: BoxFit.fill,
+                      width: 240,
+                    ),
+                    Image.asset(
+                      "assets/images/logo.png",
+                      width: 150,
+                      height: 50,
+                      fit: BoxFit.cover,
+                    ),
+                  ],
                 ),
               ),
-              child: Column(
-                children: [
-                  Image.asset(
-                    "assets/images/pan.png",
-                    height: 180,
-                    fit: BoxFit.fill,
-                    width: 240,
-                  ),
-                  Image.asset(
-                    "assets/images/logo.png",
-                    width: 150,
-                    height: 50,
-                    fit: BoxFit.cover,
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              margin: EdgeInsets.only(
-                top: MediaQuery.of(context).size.height / 3,
-                left: 20,
-                right: 20,
-              ),
-              child: Material(
-                elevation: 3,
-                borderRadius: BorderRadius.circular(20),
-                child: Container(
-                  padding: EdgeInsets.only(left: 20, right: 20),
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  height: MediaQuery.of(context).size.height / 1.65,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(height: 20),
-                      Center(
-                        child: Text(
-                          "SignUp",
-                          style: AppWidget.HeadlineTextStyle(),
-                        ),
-                      ),
-                      SizedBox(height: 30),
-                      Text("Name", style: AppWidget.SignUpTextStyle()),
-                      SizedBox(height: 5),
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Color(0xffececf8),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: "Enter Name",
-                            prefixIcon: Icon(Icons.person_outline),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                margin: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height / 3,
+                  left: 20,
+                  right: 20,
+                ),
+                child: Material(
+                  elevation: 3,
+                  borderRadius: BorderRadius.circular(20),
+                  child: Container(
+                    padding: EdgeInsets.only(left: 20, right: 20),
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    height: MediaQuery.of(context).size.height / 1.65,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 20),
+                        Center(
+                          child: Text(
+                            "SignUp",
+                            style: AppWidget.HeadlineTextStyle(),
                           ),
                         ),
-                      ),
-                      SizedBox(height: 20),
-                      Text("Email", style: AppWidget.SignUpTextStyle()),
-                      SizedBox(height: 5),
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Color(0xffececf8),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: "Enter Email",
-                            prefixIcon: Icon(Icons.mail_outline_outlined),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      Text("Password", style: AppWidget.SignUpTextStyle()),
-                      SizedBox(height: 5),
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Color(0xffececf8),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: "Enter Password",
-                            prefixIcon: Icon(Icons.password_outlined),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 30),
-                      Center(
-                        child: Container(
-                          width: 200,
-                          height: 60,
+                        SizedBox(height: 30),
+                        Text("Name", style: AppWidget.SignUpTextStyle()),
+                        SizedBox(height: 5),
+                        Container(
                           decoration: BoxDecoration(
-                            color: Color(0xffef2b39),
-                            borderRadius: BorderRadius.circular(20),
+                            color: Color(0xffececf8),
+                            borderRadius: BorderRadius.circular(10),
                           ),
-                          child: Center(
-                            child: Text(
-                              "Sign Up",
-                              style: AppWidget.BoldWhiteTextStyle(),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Enter Name",
+                              prefixIcon: Icon(Icons.person_outline),
                             ),
                           ),
                         ),
-                      ),
-                      SizedBox(height: 30),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            "Already have an account?",
-                            style: AppWidget.SimpleTextStyle(),
+                        SizedBox(height: 20),
+                        Text("Email", style: AppWidget.SignUpTextStyle()),
+                        SizedBox(height: 5),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Color(0xffececf8),
+                            borderRadius: BorderRadius.circular(10),
                           ),
-                          SizedBox(width: 10),
-                          GestureDetector(
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => Login(),
-                                ),
-                              );
-                            },
-                            child: Text(
-                              "LogIn",
-                              style: AppWidget.BoldTextStyle(),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Enter Email",
+                              prefixIcon: Icon(Icons.mail_outline_outlined),
                             ),
                           ),
-                        ],
-                      ),
-                    ],
+                        ),
+                        SizedBox(height: 20),
+                        Text("Password", style: AppWidget.SignUpTextStyle()),
+                        SizedBox(height: 5),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Color(0xffececf8),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Enter Password",
+                              prefixIcon: Icon(Icons.password_outlined),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 30),
+                        Center(
+                          child: Container(
+                            width: 200,
+                            height: 60,
+                            decoration: BoxDecoration(
+                              color: Color(0xffef2b39),
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Center(
+                              child: Text(
+                                "Sign Up",
+                                style: AppWidget.BoldWhiteTextStyle(),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 30),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Already have an account?",
+                              style: AppWidget.SimpleTextStyle(),
+                            ),
+                            SizedBox(width: 10),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => Login(),
+                                  ),
+                                );
+                              },
+                              child: Text(
+                                "LogIn",
+                                style: AppWidget.BoldTextStyle(),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
