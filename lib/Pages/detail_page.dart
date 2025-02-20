5ba29d6b39082374deed50103b04f9653752465d
Misbah Haque <misbahmuneer2003@gmail.com>
2025-02-20 16:36:14 +0500

    Update

--- lib/Pages/detail_page.dart
+++ lib/Pages/detail_page.dart
@@ -52,148 +52,158 @@ class _DetailPageState extends State<DetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        margin: EdgeInsets.only(top: 50, left: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            GestureDetector(
-              onTap: () {
-                Navigator.pop(context);
-              },
-              child: Container(
-                padding: EdgeInsets.all(5),
-                child: Icon(
-                  Icons.arrow_back_ios_new_rounded,
-                  size: 30,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-            SizedBox(height: 10),
-            Center(
-              child: Image.asset(
-                widget.image!,
-                height: MediaQuery.of(context).size.height / 3,
-                fit: BoxFit.contain,
-              ),
-            ),
-            SizedBox(height: 20),
-            Text(widget.name!, style: AppWidget.HeadlineTextStyle()),
-            Text("Rs " + widget.price!, style: AppWidget.PriceTextStyle()),
-            SizedBox(height: 30),
-            Padding(
-              padding: const EdgeInsets.only(right: 10),
-              child: Text(widget.desc!, style: AppWidget.SimpleTextStyle()),
-            ),
-            SizedBox(height: 30),
-            Text("Quantity", style: AppWidget.PriceTextStyle()),
-            SizedBox(height: 10),
-            Row(
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    quantity = quantity + 1;
-                    totalprice = totalprice + int.parse(widget.price!);
-                    setState(() {});
-                  },
-                  child: Material(
-                    elevation: 3,
-                    borderRadius: BorderRadius.circular(10),
-                    child: Container(
-                      padding: EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                        color: Color(0xffef2b39),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Icon(Icons.add, color: Colors.white, size: 30),
-                    ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Container(
+          margin: EdgeInsets.only(top: 50, left: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: Container(
+                  padding: EdgeInsets.all(5),
+                  child: Icon(
+                    Icons.arrow_back_ios_new_rounded,
+                    size: 30,
+                    color: Colors.black,
                   ),
                 ),
-                SizedBox(width: 20),
-                Text(quantity.toString(), style: AppWidget.HeadlineTextStyle()),
-                SizedBox(width: 20),
-                GestureDetector(
-                  onTap: () {
-                    if (quantity > 1) {
-                      quantity = quantity - 1;
-                      totalprice = totalprice - int.parse(widget.price!);
+              ),
+              SizedBox(height: 10),
+              Center(
+                child: Image.asset(
+                  widget.image!,
+                  height: MediaQuery.of(context).size.height / 3,
+                  fit: BoxFit.contain,
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(widget.name!, style: AppWidget.HeadlineTextStyle()),
+              Text("Rs " + widget.price!, style: AppWidget.PriceTextStyle()),
+              SizedBox(height: 30),
+              Padding(
+                padding: const EdgeInsets.only(right: 10),
+                child: Text(widget.desc!, style: AppWidget.SimpleTextStyle()),
+              ),
+              SizedBox(height: 30),
+              Text("Quantity", style: AppWidget.PriceTextStyle()),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      quantity = quantity + 1;
+                      totalprice = totalprice + int.parse(widget.price!);
                       setState(() {});
-                    }
-                  },
-                  child: Material(
-                    elevation: 3,
-                    borderRadius: BorderRadius.circular(10),
-                    child: Container(
-                      padding: EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                        color: Color(0xffef2b39),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Icon(Icons.remove, color: Colors.white, size: 30),
-                    ),
-                  ),
-                ),
-                SizedBox(width: 20),
-              ],
-            ),
-            SizedBox(height: 40),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Material(
-                  elevation: 3,
-                  borderRadius: BorderRadius.circular(20),
-                  child: Container(
-                    height: 60,
-                    width: 120,
-                    decoration: BoxDecoration(
-                      color: Color(0xffef2b39),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Center(
-                      child: Text(
-                        "Rs " + totalprice.toString(),
-                        style: AppWidget.BoldWhiteTextStyle(),
+                    },
+                    child: Material(
+                      elevation: 3,
+                      borderRadius: BorderRadius.circular(10),
+                      child: Container(
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          color: Color(0xffef2b39),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Icon(Icons.add, color: Colors.white, size: 30),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(width: 30),
-                GestureDetector(
-                  onTap: () {
-                    print("Checking address...");
-                    if (address == null) {
-                      print("No address found, opening address box...");
-                      openBox();
-                    } else {
-                      print("Address exists: $address");
-                      print("Order Now pressed, proceeding to payment...");
-                      makePayment(totalprice.toString());
-                    }
-                  },
-                  child: Material(
-                    elevation: 3,
-                    borderRadius: BorderRadius.circular(20),
-                    child: Container(
-                      height: 70,
-                      width: 200,
-                      decoration: BoxDecoration(
-                        color: Colors.black,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Center(
-                        child: Text(
-                          "Order Now",
-                          style: AppWidget.WhiteTextStyle(),
+                  SizedBox(width: 20),
+                  Text(
+                    quantity.toString(),
+                    style: AppWidget.HeadlineTextStyle(),
+                  ),
+                  SizedBox(width: 20),
+                  GestureDetector(
+                    onTap: () {
+                      if (quantity > 1) {
+                        quantity = quantity - 1;
+                        totalprice = totalprice - int.parse(widget.price!);
+                        setState(() {});
+                      }
+                    },
+                    child: Material(
+                      elevation: 3,
+                      borderRadius: BorderRadius.circular(10),
+                      child: Container(
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          color: Color(0xffef2b39),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Icon(
+                          Icons.remove,
+                          color: Colors.white,
+                          size: 30,
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  SizedBox(width: 20),
+                ],
+              ),
+              SizedBox(height: 40),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Material(
+                    elevation: 3,
+                    borderRadius: BorderRadius.circular(20),
+                    child: Container(
+                      height: 60,
+                      width: 120,
+                      decoration: BoxDecoration(
+                        color: Color(0xffef2b39),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Rs " + totalprice.toString(),
+                          style: AppWidget.BoldWhiteTextStyle(),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 30),
+                  GestureDetector(
+                    onTap: () {
+                      print("Checking address...");
+                      if (address == null) {
+                        print("No address found, opening address box...");
+                        openBox();
+                      } else {
+                        print("Address exists: $address");
+                        print("Order Now pressed, proceeding to payment...");
+                        makePayment(totalprice.toString());
+                      }
+                    },
+                    child: Material(
+                      elevation: 3,
+                      borderRadius: BorderRadius.circular(20),
+                      child: Container(
+                        height: 70,
+                        width: 200,
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Order Now",
+                            style: AppWidget.WhiteTextStyle(),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
