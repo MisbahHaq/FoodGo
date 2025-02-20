820d56e07123c8d57cce78cdd92b08e60429cce0
Misbah Haque <misbahmuneer2003@gmail.com>
2025-02-20 14:25:38 +0500

    Update

--- lib/Pages/wallet.dart
+++ lib/Pages/wallet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:foodgo/Service/widget_support.dart';
 
 class WalletPage extends StatefulWidget {
   const WalletPage({super.key});
@@ -10,6 +11,50 @@ class WalletPage extends StatefulWidget {
 class _WalletPageState extends State<WalletPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.only(top: 40),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [Text("Wallet", style: AppWidget.HeadlineTextStyle())],
+            ),
+            SizedBox(height: 10),
+            Expanded(
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: Color(0xffececf8),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    SizedBox(height: 20),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(color: Colors.white),
+                      child: Row(
+                        children: [
+                          Image.asset(
+                            "assets/images/wallet.png",
+                            height: 50,
+                            width: 50,
+                            fit: BoxFit.contain,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
