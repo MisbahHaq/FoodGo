6343e01383e4c67a4904b099dcb05b48665e945f
Misbah Haque <misbahmuneer2003@gmail.com>
2025-02-20 16:31:44 +0500

    Update

--- lib/Service/mexican_data.dart
+++ lib/Service/mexican_data.dart
@@ -13,7 +13,7 @@ List<MexicanModel> getMexican() {
   mexicanModel.image = "assets/images/chilaquiles.png";
   mexicanModel.price = "850";
   mexicanModel.desc =
-      "A Cheese Burger features a juicy, seasoned patty topped with melted cheese, fresh lettuce, tomatoes, and pickles, all packed in a soft, toasted bun for a rich and savory bite.";
+      "Crispy tortilla chips simmered in red or green sauce, topped with cheese, crema, a fried egg, and garnished with cilantro and onions. A delicious, savory Mexican breakfast dish full of flavor and texture.";
   mexican.add(mexicanModel);
   mexicanModel = new MexicanModel();
 
@@ -21,7 +21,7 @@ List<MexicanModel> getMexican() {
   mexicanModel.image = "assets/images/fresh-tacos.png";
   mexicanModel.price = "1250";
   mexicanModel.desc =
-      "Veggie Pizza features a crispy crust topped with rich tomato sauce, melted cheese, and a colorful mix of fresh vegetables for a flavorful and wholesome bite.";
+      "Soft corn tortillas filled with your choice of seasoned meat, topped with fresh salsa, cilantro, onions, and a squeeze of lime. A simple, vibrant, and satisfying Mexican classic.";
   mexican.add(mexicanModel);
   mexicanModel = new MexicanModel();
 
@@ -29,7 +29,7 @@ List<MexicanModel> getMexican() {
   mexicanModel.image = "assets/images/burritos.png";
   mexicanModel.price = "1150";
   mexicanModel.desc =
-      "A Cheese Burger features a juicy, seasoned patty topped with melted cheese, fresh lettuce, tomatoes, and pickles, all packed in a soft, toasted bun for a rich and savory bite.";
+      "Flour tortillas wrapped around a hearty filling of seasoned meat, beans, rice, cheese, and fresh toppings. A flavorful and filling Mexican favorite, perfect for on-the-go or a satisfying meal.";
   mexican.add(mexicanModel);
   mexicanModel = new MexicanModel();
 
@@ -45,7 +45,7 @@ List<MexicanModel> getMexican() {
   mexicanModel.image = "assets/images/salsa.png";
   mexicanModel.price = "1000";
   mexicanModel.desc =
-      "Veggie Pizza features a crispy crust topped with rich tomato sauce, melted cheese, and a colorful mix of fresh vegetables for a flavorful and wholesome bite.";
+      "Fried eggs served on warm corn tortillas, topped with a zesty tomato-based salsa, and garnished with fresh cilantro, onions, and cheese. A classic, hearty Mexican breakfast bursting with flavor.";
   mexican.add(mexicanModel);
   mexicanModel = new MexicanModel();
 
@@ -53,7 +53,7 @@ List<MexicanModel> getMexican() {
   mexicanModel.image = "assets/images/trad.png";
   mexicanModel.price = "1000";
   mexicanModel.desc =
-      "Veggie Pizza features a crispy crust topped with rich tomato sauce, melted cheese, and a colorful mix of fresh vegetables for a flavorful and wholesome bite.";
+      "A vibrant blend of bold flavors and fresh ingredients, featuring dishes like tacos, tamales, enchiladas, and salsas. Rich in history and culture, each dish is crafted with a perfect balance of spices, herbs, and textures, offering a true taste of Mexico's culinary heritage.";
   mexican.add(mexicanModel);
   mexicanModel = new MexicanModel();
 
@@ -69,7 +69,7 @@ List<MexicanModel> getMexican() {
   mexicanModel.image = "assets/images/yakitori.png";
   mexicanModel.price = "1250";
   mexicanModel.desc =
-      "Veggie Pizza features a crispy crust topped with rich tomato sauce, melted cheese, and a colorful mix of fresh vegetables for a flavorful and wholesome bite.";
+      "Grilled skewered chicken, marinated in a savory tare sauce, with a perfect balance of sweet and salty flavors. Often served with scallions or vegetables, this Japanese street food is tender, smoky, and delicious.";
   mexican.add(mexicanModel);
   mexicanModel = new MexicanModel();
 
