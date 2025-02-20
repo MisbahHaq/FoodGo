1b2b5a73645994d07e967694d42fd0b050dd10f2
Misbah Haque <misbahmuneer2003@gmail.com>
2025-02-20 10:46:28 +0500

    Update

--- lib/Pages/order.dart
+++ lib/Pages/order.dart
@@ -21,33 +21,36 @@ class _OrderPageState extends State<OrderPage> {
               children: [Text("Orders", style: AppWidget.HeadlineTextStyle())],
             ),
             SizedBox(height: 10),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                color: Color(0xffececf8),
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
-                ),
-              ),
-              child: Column(
-                children: [
-                  SizedBox(height: 20),
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(color: Colors.white),
-                    child: Row(
-                      children: [
-                        Image.asset(
-                          "assets/images/burger1.png",
-                          height: 50,
-                          width: 50,
-                          fit: BoxFit.cover,
-                        ),
-                      ],
-                    ),
+            Expanded(
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: Color(0xffececf8),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
                   ),
-                ],
+                ),
+                child: Column(
+                  children: [
+                    SizedBox(height: 20),
+                    Container(
+                      margin: EdgeInsets.only(left: 20, right: 20),
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(color: Colors.white),
+                      child: Row(
+                        children: [
+                          Image.asset(
+                            "assets/images/burger1.png",
+                            height: 120,
+                            width: 120,
+                            fit: BoxFit.cover,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
