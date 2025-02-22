775da8f65f6a615c8427bb4b34f1b0ac9dcdd7c8
Misbah Haque <misbahmuneer2003@gmail.com>
2025-02-22 12:23:18 +0500

    Update

--- lib/Admin/home_admin.dart
+++ lib/Admin/home_admin.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:foodgo/Admin/all_orders.dart';
+import 'package:foodgo/Admin/manage_users.dart';
 import 'package:foodgo/Service/widget_support.dart';
 
 class HomeAdmin extends StatefulWidget {
@@ -13,30 +15,19 @@ class _HomeAdminState extends State<HomeAdmin> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        margin: EdgeInsets.only(top: 40),
+        margin: EdgeInsets.only(top: 60),
         child: Column(
           children: [
             Padding(
               padding: const EdgeInsets.only(left: 20),
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Container(
-                    padding: EdgeInsets.all(8),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                      color: Color(0xffef2b39),
-                    ),
-                    child: Icon(
-                      Icons.arrow_back_ios_new_rounded,
-                      color: Colors.white,
-                    ),
-                  ),
-
                   Text("Home Admin", style: AppWidget.HeadlineTextStyle()),
                 ],
               ),
             ),
-            SizedBox(height: 20),
+            SizedBox(height: 40),
             Expanded(
               child: Container(
                 width: MediaQuery.of(context).size.width,
@@ -49,14 +40,128 @@ class _HomeAdminState extends State<HomeAdmin> {
                 ),
                 child: Column(
                   children: [
-                    SizedBox(height: 21),
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      child: Image.asset(
-                        "assets/images/delivery-man.png",
-                        height: 50,
-                        width: 50,
-                        fit: BoxFit.contain,
+                    SizedBox(height: 81),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => AllOrders()),
+                        );
+                      },
+                      child: Container(
+                        margin: EdgeInsets.only(right: 20, left: 20),
+                        child: Material(
+                          elevation: 3,
+                          borderRadius: BorderRadius.circular(20),
+                          child: Container(
+                            padding: EdgeInsets.only(
+                              left: 20,
+                              right: 20,
+                              top: 10,
+                              bottom: 10,
+                            ),
+
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            width: MediaQuery.of(context).size.width,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Image.asset(
+                                  "assets/images/delivery-man.png",
+                                  height: 120,
+                                  width: 120,
+                                  fit: BoxFit.contain,
+                                ),
+                                Text(
+                                  "Manage\nOrders",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(5),
+                                  decoration: BoxDecoration(
+                                    color: Color(0xffef2b39),
+                                    borderRadius: BorderRadius.circular(30),
+                                  ),
+                                  child: Icon(
+                                    Icons.arrow_forward_ios_rounded,
+                                    color: Colors.white,
+                                    size: 30,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 50),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ManageUsers(),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        margin: EdgeInsets.only(right: 20, left: 20),
+                        child: Material(
+                          elevation: 3,
+                          borderRadius: BorderRadius.circular(20),
+                          child: Container(
+                            padding: EdgeInsets.only(
+                              left: 20,
+                              right: 20,
+                              top: 10,
+                              bottom: 10,
+                            ),
+
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            width: MediaQuery.of(context).size.width,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Image.asset(
+                                  "assets/images/team.png",
+                                  height: 120,
+                                  width: 120,
+                                  fit: BoxFit.contain,
+                                ),
+                                Text(
+                                  "Manage\nUsers",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(5),
+                                  decoration: BoxDecoration(
+                                    color: Color(0xffef2b39),
+                                    borderRadius: BorderRadius.circular(30),
+                                  ),
+                                  child: Icon(
+                                    Icons.arrow_forward_ios_rounded,
+                                    color: Colors.white,
+                                    size: 30,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
                       ),
                     ),
                   ],
